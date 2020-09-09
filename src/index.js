1329852d0e9813f8e3722861eecc40bb8e7ad5d8
Sirozu <43061026+Sirozu@users.noreply.github.com>
2020-09-09 16:56:03 +0300

    1 - 6 сделаны
    Пункты 1 - 6  сделаны

--- src/index.js
+++ src/index.js
@@ -22,17 +22,11 @@ function draw(tFrame) {
 
     drawPlatform(context)
     drawBall(context)
+    drawScore(context)
+    
 }
 
-function update(tick) {
 
-    const vx = (gameState.pointer.x - gameState.player.x) / 10
-    gameState.player.x += vx
-
-    const ball = gameState.ball
-    ball.y += ball.vy
-    ball.y += ball.vx
-}
 
 function run(tFrame) {
     gameState.stopCycle = window.requestAnimationFrame(run);
@@ -49,8 +43,8 @@ function run(tFrame) {
     gameState.lastRender = tFrame;
 }
 
-function stopGame(handle) {
-    window.cancelAnimationFrame(handle);
+function stopGame() {
+    window.cancelAnimationFrame(gameState.stopCycle);
 }
 
 function drawPlatform(context) {
@@ -69,6 +63,14 @@ function drawBall(context) {
     context.fillStyle = "#0000FF";
     context.fill();
     context.closePath();
+}
+
+function drawScore(context) {
+	context.fillStlye = "black";
+	context.font = "16px Arial, sans-serif";
+	context.textAlign = "left";
+	context.textBaseline = "top";
+	context.fillText("Score: " + gameState.score, 20, 20 );
 }
 
 function setup() {
@@ -101,9 +103,102 @@ function setup() {
         y: 0,
         radius: 25,
         vx: 0,
-        vy: 5
+        vy: 5,
+        angle : 0
+    }
+
+    gameState.particlePos = {}
+    gameState.score = 0
+}
+
+
+function update(tick) {
+
+    
+    const vx = (gameState.pointer.x - gameState.player.x) // 10
+    gameState.player.x += vx
+
+    const ball = gameState.ball
+
+    if ((collision(gameState.ball, gameState.player) && ball.vy > 0)) {
+        collideAction(gameState.ball, gameState.player)
+    }
+
+    ball.y += ball.vy
+    ball.x += ball.vx
+
+    if(ball.y + ball.radius > canvas.height) {
+        ball.y = canvas.height - ball.radius;
+        stopGame();
+    } 
+        
+    if(ball.y < 0) {
+        ball.vy = -ball.vy;
+        ball.y = ball.radius;
+    }
+
+    if(ball.x + ball.radius > canvas.width) {
+        ball.vx = -ball.vx;
+        ball.x = canvas.width - ball.radius;
+    }
+    
+    else if(ball.x -ball.radius < 0) {
+        ball.vx = -ball.vx;
+        ball.x = ball.radius;
     }
 }
+
+function collision(b, p) {
+    if(b.x + gameState.ball.radius >= p.x - p.width/2
+        && b.x - gameState.ball.radius <=(p.x - p.width/2 + p.width)) {
+		if(b.y >= (p.y - p.height) && p.y > 0){
+			
+			return true;
+		}
+		
+		else if(b.y <= p.height && p.y == 0) {
+			
+			return true;
+		}
+		
+		else return false;
+	}
+}
+
+function collideAction(ball, p) {
+	//ball.vy = -ball.vy;
+	gameState.score += 1
+    ball.y = p.y - p.height;
+    gameState.particlePos.y = ball.y + ball.radius;
+    gameState.particlePos.x = ball.x;
+    
+    changeAngle(90 - ball.angle); 
+
+}
+
+
+function changeAngle(angle) {
+    if(angle == 0) angle = 1; 
+    angle = angle;
+    radians = angle / (180 * Math.PI) * 10;
+    //console.log(Math.cos(radians))
+ 
+    gameState.ball.vx = -(Math.cos(radians) * gameState.ball.vx) + getRandomArbitrary(-5, 5);
+    gameState.ball.vy = -(Math.sin(radians) * gameState.ball.vy);
+    if (score % 30 > 0)
+        gameState.ball.vy += 1
+  }
+
+ function angleTo(x, y) {
+    changeAngle(Math.atan2(y - y, x - x));
+  }
+
+  function getRandomArbitrary(min, max) {
+      
+      let a = Math.random() * (max - min) + min
+      console.log(a)
+    return a
+  }
 
 setup();
 run();
